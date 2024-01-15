27d87227c9b2a1f5d710b2e4fea8bd941cfe5a4a
João Victor de Oliveira <120110746+JoaoVictorDevRN@users.noreply.github.com>
2024-01-14 23:09:07 -0300

    main.py
    Melhor comentado

--- ExtrairPDF/main.py
+++ ExtrairPDF/main.py
@@ -1,9 +1,18 @@
+# Importa a biblioteca PyPDF2 para trabalhar com arquivos PDF
 import PyPDF2
 
+# Abre o arquivo PDF no modo de leitura binária ('rb')
 pdf = open('Teste.pdf', 'rb')
-reader = PyPDF2.PdfFileReader(pdf)
-pagina = reader.getPage(0)  # Use getPage para obter a página
 
+# Cria um objeto PdfFileReader para ler o arquivo PDF
+reader = PyPDF2.PdfFileReader(pdf)
+
+# Usa o método getPage para obter a primeira página do PDF
+pagina = reader.getPage(0)
+
+# Imprime o texto extraído da primeira página
 print(pagina.extractText())
 
-pdf.close()  # Não se esqueça de fechar o arquivo PDF após a leitura
+# Fecha o arquivo PDF após a leitura
+pdf.close()
+
